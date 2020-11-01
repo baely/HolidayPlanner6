729bc485141cae5000f6753fc6bcde7aca82770a
baely <bailey_fun@live.com.au>
2020-11-01 18:31:57 +1100

    Replaced built ORM with SQLAlchemy

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 import sys
 
 from flask import Flask, request
+from sqlalchemy.orm import sessionmaker
 
-from db import DB, DBObject
 from extension import Response
-from plan import Plan, PlanItemGeneric, PlanItemHotel
-from plan.peripheral import Location, PointOfInterest, Hotel
+from plan import engine, Plan, PlanItemGeneric, PlanItemHotel, Location, PointOfInterest, Hotel
 
 app = Flask(__name__)
-db = DB()
+Session = sessionmaker(bind=engine)
+session = Session()
 
 
 @app.route("/")
@@ -18,13 +18,13 @@ def index():
 
 @app.route("/api/plan/", methods=["GET"])
 def list_plans():
-    plan_list = Plan.list_all(db)
+    plan_list = Plan.list_all()
     return Response(plan_list).as_response()
 
 
 @app.route("/api/plan/<plan_id>", methods=["GET"])
 def get_plan(plan_id):
-    p = Plan.load(db, plan_id)
+    p = Plan.load(plan_id)
     return Response(p).as_response()
 
 
@@ -33,36 +33,22 @@ def create():
     body = request.get_json()
     if "id" in body.keys():
         del body["id"]
-    p = Plan.from_object(body, db)
+    p = Plan.from_object(body)
     return Response(p).as_response()
 
 
 @app.route("/api/plan/<plan_id>", methods=["POST"])
 def save(plan_id):
     body = request.get_json()
-    p = Plan.from_object(body, db, plan_id)
+    p = Plan.from_object(body, plan_id)
     p.save()
     return Response(p).as_response()
 
 
 if __name__ == '__main__':
     if len(sys.argv) > 1 and sys.argv[1] == "setup":
-        db.setup()
+        # db.setup()
         # plan = Plan.get_by_id(1)
-
-        melbourne_location = Location(x=-37.8137564, y=144.9621251)
-        melbourne_poi = PointOfInterest(label="Melbourne", location=melbourne_location)
-        melbourne_plan = Plan(poi=melbourne_poi)
-
-        print("Melbourne Coordinates", melbourne_location.__dict__)
-        print("Melbourne POI", melbourne_poi.__dict__)
-        print("Holiday to Melbourne", melbourne_plan.__dict__)
-
-        generic_plan = PlanItemGeneric(label="Generic Plan", start_time="never.")
-        hotel_item = PlanItemHotel(hotel=Hotel(name="Big Hotel", location=melbourne_location), check_in="now", checkout="later")
-
-        print(generic_plan.__dict__)
-        print(type(generic_plan).__bases__)
-
+        pass
     else:
         app.run()
--- plan/__init__.py
+++ plan/__init__.py
@@ -2,29 +2,124 @@ from abc import ABC
 from datetime import datetime
 from typing import List
 
-from db import DBObject
-from plan.peripheral import Flight, Hotel, Location, PointOfInterest
+from sqlalchemy import create_engine, Column, DateTime, Integer, Float, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
 
 
-class PlanItemBase(DBObject):
-    start_time: str
-    pass
+engine = create_engine("postgresql://postgres:100598@localhost:5432/postgres", echo=True)
+Base = declarative_base()
+
+
+class Location(Base):
+    __tablename__ = "Location"
+    id: int = Column(Integer, primary_key=True)
+
+    x: float = Column(Float)
+    y: float = Column(Float)
+
+
+class Airport(Base):
+    __tablename__ = "Airport"
+    id: int = Column(Integer, primary_key=True)
+
+    name: str = Column(String)
+    abbr: str = Column(String)
+    location_id: int = Column(Integer, ForeignKey('Location.id'))
+    location: Location = relationship(Location)
+
+
+class FlightPoint(Base):
+    __tablename__ = "FlightPoint"
+    id: int = Column(Integer, primary_key=True)
+
+    airport_id: int = Column(Integer, ForeignKey('Airport.id'))
+    airport: Airport = relationship(Airport)
+    time: datetime = Column(DateTime)
+
+
+class Flight(Base):
+    __tablename__ = "Flight"
+    id: int = Column(Integer, primary_key=True)
+
+    flight_plan: int = Column(Integer, ForeignKey('PlanItemFlight.id'))
+    departure_id: int = Column(Integer, ForeignKey('FlightPoint.id'))
+    departure: FlightPoint = relationship(FlightPoint)
+    arrival_id: int = Column(Integer, ForeignKey('FlightPoint.id'))
+    arrival: FlightPoint = relationship(FlightPoint)
+
+
+class PointOfInterest(Base):
+    __tablename__ = "PointOfInterest"
+    id: int = Column(Integer, primary_key=True)
+
+    label: str = Column(String)
+    location_id: int = Column(Integer, ForeignKey('Location.id'))
+    location: Location = relationship(Location)
+
+
+class Hotel(Base):
+    __tablename__ = "Hotel"
+    id: int = Column(Integer, primary_key=True)
+
+    name: str = Column(String)
+    location_id: int = Column(Integer, ForeignKey('Location.id'))
+    location: Location = relationship(Location)
+
+
+class PlanItemBase(Base):
+    __tablename__ = "PlanItemBase"
+    id: int = Column(Integer, primary_key=True)
+
+    start_time: str = Column(String)
+    plan: int = Column(Integer, ForeignKey('Plan.id'))
+
+    discriminator = Column('type', String)
+    __mapper_args__ = {'polymorphic_on': discriminator}
 
 
 class PlanItemFlight(PlanItemBase):
-    flights: List[Flight]
+    __tablename__ = "PlanItemFlight"
+    id: int = Column(Integer, ForeignKey('PlanItemBase.id'), primary_key=True)
+
+    flights: List[Flight] = relationship(Flight)
+
+    __mapper_args__ = {
+        'polymorphic_identity': 'PlanItemFlight',
+    }
 
 
 class PlanItemGeneric(PlanItemBase):
-    label: str
+    __tablename__ = "PlanItemGeneric"
+    id: int = Column(Integer, ForeignKey('PlanItemBase.id'), primary_key=True)
+
+    label: str = Column(String)
+
+    __mapper_args__ = {
+        'polymorphic_identity': 'PlanItemGeneric',
+    }
 
 
 class PlanItemHotel(PlanItemBase):
+    __tablename__ = "PlanItemHotel"
+    id: int = Column(Integer, ForeignKey('PlanItemBase.id'), primary_key=True)
+
     hotel: Hotel
-    check_in: str
-    check_out: str
+    check_in: datetime = Column(DateTime)
+    check_out: datetime = Column(DateTime)
+
+    __mapper_args__ = {
+        'polymorphic_identity': 'PlanItemHotel',
+    }
 
 
-class Plan(DBObject):
-    poi: PointOfInterest
-    items: List[PlanItemBase]
+class Plan(Base):
+    __tablename__ = "Plan"
+    id: int = Column(Integer, primary_key=True)
+
+    poi_id: int = Column(Integer, ForeignKey('PointOfInterest.id'))
+    poi: PointOfInterest = relationship(PointOfInterest)
+    items: List[PlanItemBase] = relationship(PlanItemBase)
+
+
+Base.metadata.create_all(engine)
